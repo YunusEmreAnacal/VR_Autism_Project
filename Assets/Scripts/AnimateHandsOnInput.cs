fb3aabef155da190a0370b58fc6d31af1b4705c6
Yunus Emre Anacal <115077396+YunusEmreAnacal@users.noreply.github.com>
2023-11-12 15:04:19 +0300

    push project

---
+++ Assets/Scripts/AnimateHandsOnInput.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AnimateHandsOnInput : MonoBehaviour
+{
+    public InputActionProperty pinchAnimationAction;
+    public InputActionProperty gripAnimationAction;
+    public Animator handAnimator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float triggerValue = pinchAnimationAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Trigger", triggerValue);
+
+        float gripValue = gripAnimationAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Grip", gripValue);
+    }
+}
