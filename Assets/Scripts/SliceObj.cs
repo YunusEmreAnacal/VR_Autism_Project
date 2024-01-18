6434a76f364892e6cb836783335613ebdc97c3e4
Yunus Emre Anacal <115077396+YunusEmreAnacal@users.noreply.github.com>
2024-01-18 16:39:53 +0300

    ReadyForJury

--- Assets/Scripts/SliceObj.cs
+++ Assets/Scripts/SliceObj.cs
@@ -6,22 +6,41 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class SliceObj : MonoBehaviour
 {
-    public Material materialSlicedSide;
+    public Material bread;
+    public Material lemon;
+    public Material tomato;
     public float explosionForce;
     public float exposionRadius;
     public bool gravity, kinematic;
+
+    private Material x;
 
     private void OnTriggerEnter(Collider other)
     {
 
         if (other.gameObject.CompareTag("CanSlice"))
         {
-            SlicedHull sliceobj = Slice(other.gameObject, materialSlicedSide);
-            GameObject SlicedObjtop = sliceobj.CreateUpperHull(other.gameObject, materialSlicedSide);
-            GameObject SliceObjDown = sliceobj.CreateLowerHull(other.gameObject, materialSlicedSide);
-            Destroy(other.gameObject);
-            AddComponent(SlicedObjtop);
-            AddComponent(SliceObjDown);
+            if (other.name.Contains("Bread"))
+            {
+                x = bread;
+                SlicedHull sliceobj = Slice(other.gameObject, x);
+                GameObject SlicedObjtop = sliceobj.CreateUpperHull(other.gameObject, x);
+                GameObject SliceObjDown = sliceobj.CreateLowerHull(other.gameObject, x);
+                Destroy(other.gameObject);
+                AddComponentForBread(SlicedObjtop);
+                AddComponentForBread(SliceObjDown);
+            }
+            else if (other.name.Contains("Lemon"))
+            {
+                x = lemon;
+                SlicedHull sliceobj = Slice(other.gameObject, x);
+                GameObject SlicedObjtop = sliceobj.CreateUpperHull(other.gameObject, x);
+                GameObject SliceObjDown = sliceobj.CreateLowerHull(other.gameObject, x);
+                Destroy(other.gameObject);
+                AddComponentForLemon(SlicedObjtop);
+                AddComponentForLemon(SliceObjDown);
+            }
+            
         }
     }
 
@@ -29,7 +48,8 @@ public class SliceObj : MonoBehaviour
     {
         return obj.Slice(transform.position, transform.forward, mat);
     }
-    void AddComponent (GameObject obj){
+
+    void AddComponentForBread (GameObject obj){
         obj.AddComponent<BoxCollider>();
         var rigidbody = obj.AddComponent<Rigidbody>();
         rigidbody.useGravity = gravity;
@@ -38,8 +58,26 @@ public class SliceObj : MonoBehaviour
         //Destroy(obj,3f);
         XRGrabInteractable script = obj.AddComponent<XRGrabInteractable>();
         obj.tag= "CanSlice";
+        obj.name = "Bread";
+        
         script.selectMode = InteractableSelectMode.Multiple;
         script.useDynamicAttach = true;
        
+    }
+    void AddComponentForLemon(GameObject obj)
+    {
+        obj.AddComponent<BoxCollider>();
+        var rigidbody = obj.AddComponent<Rigidbody>();
+        rigidbody.useGravity = gravity;
+        rigidbody.isKinematic = kinematic;
+        rigidbody.AddExplosionForce(explosionForce, obj.transform.position, exposionRadius);
+        //Destroy(obj,3f);
+        XRGrabInteractable script = obj.AddComponent<XRGrabInteractable>();
+        obj.tag = "CanSlice";
+        obj.name = "Lemon";
+
+        script.selectMode = InteractableSelectMode.Multiple;
+        script.useDynamicAttach = true;
+
     }
 }
