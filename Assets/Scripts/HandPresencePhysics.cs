6fc4c80a31e933925450ac59c421969a61b15b71
Yunus Emre Anacal <115077396+YunusEmreAnacal@users.noreply.github.com>
2024-03-18 17:52:01 +0300

    collider added to hands

--- Assets/Scripts/HandPresencePhysics.cs
+++ Assets/Scripts/HandPresencePhysics.cs
@@ -14,7 +14,7 @@ public class HandPresencePhysics : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         rb.velocity = (target.position - transform.position) / Time.fixedDeltaTime;
 
--- Assets/Scripts/SliceObj.cs
+++ Assets/Scripts/SliceObj.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using EzySlice;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR;
 
 public class SliceObj : MonoBehaviour
 {
@@ -13,21 +14,35 @@ public class SliceObj : MonoBehaviour
     public float exposionRadius;
     public bool gravity, kinematic;
 
+    private InputDevice leftController;
+    private InputDevice rightController;
+
     public bool isInsideTrigger = false;
 
     public float slicingSpeedThreshold = 0.5f; // Örnek eþik deðeri
 
     public Rigidbody knifeRigidbody;
 
-    AudioSource source;
+    public AudioClip sesDosyasi1; // Trigger alaný 1 için ses dosyasý
+    public AudioClip sesDosyasi2; // Trigger alaný 2 için ses dosyasý
+    AudioSource source1;
 
 
 
     private Material x;
 
+    void Start()
+    {
+        // Sol ve sað kontrol cihazlarýný al
+        InputDeviceCharacteristics controllerCharacteristics = InputDeviceCharacteristics.Controller | InputDeviceCharacteristics.Left | InputDeviceCharacteristics.Right;
+        leftController = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
+        rightController = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
+    }
+
     private void Awake()
     {
-        source = GetComponent<AudioSource>();
+        source1 = GetComponent<AudioSource>();
+
     }
 
     private void OnTriggerEnter(Collider other)
@@ -38,13 +53,32 @@ public class SliceObj : MonoBehaviour
 
             Debug.Log("Nesne trigger alanýna girdi.");
             //    isInsideTrigger = true;
-             other.GetComponent<Rigidbody>().isKinematic = true;
-             other.GetComponent<BoxCollider>().isTrigger = true;
-             source.Play();
+            //other.GetComponent<Rigidbody>().isKinematic = true;
+            //other.GetComponent<BoxCollider>().isTrigger = true;
+             source1.clip = sesDosyasi1;
+             source1.Play();
 
+
+        }
+        if (other.gameObject.CompareTag("Hands"))
+        {
+            source1.clip = sesDosyasi2;
+            source1.Play();
+            // Örneðin, sol kolu titreþtir
+            if (leftController != null)
+            {
+                HapticFeedback(leftController, 1f, 0.5f);
+            }
+
+            // Örneðin, sað kolu titreþtir
+            if (rightController != null)
+            {
+                HapticFeedback(rightController, 1f, 0.5f);
+            }
         }
        
     }
+
 
 
 
@@ -67,6 +101,8 @@ public class SliceObj : MonoBehaviour
                     Destroy(other.gameObject);
                     AddComponentForBread(SlicedObjtop);
                     AddComponentForBread(SliceObjDown);
+                    //other.GetComponent<Rigidbody>().isKinematic = false;
+                    //other.GetComponent<BoxCollider>().isTrigger = false;
                 }
                 else if (other.name.Contains("Lemon"))
                 {
@@ -77,6 +113,8 @@ public class SliceObj : MonoBehaviour
                     Destroy(other.gameObject);
                     AddComponentForLemon(SlicedObjtop);
                     AddComponentForLemon(SliceObjDown);
+                    //other.GetComponent<Rigidbody>().isKinematic = false;
+                    //other.GetComponent<BoxCollider>().isTrigger = false;
                 }
                 else if (other.name.Contains("Watermelon"))
                 {
@@ -87,19 +125,18 @@ public class SliceObj : MonoBehaviour
                     Destroy(other.gameObject);
                     AddComponentForWatermelon(SlicedObjtop);
                     AddComponentForWatermelon(SliceObjDown);
+                    //other.GetComponent<Rigidbody>().isKinematic = false;
+                    //other.GetComponent<BoxCollider>().isTrigger = false;
                 }
-                other.GetComponent<Rigidbody>().isKinematic = false;
-                other.GetComponent<BoxCollider>().isTrigger = false;
+                
             }
 
 
-             source.Stop();
+             source1.Stop();
             
             //isInsideTrigger = false;
 
         }
-
-
 
     }
 
@@ -121,7 +158,10 @@ public class SliceObj : MonoBehaviour
         
         script.selectMode = InteractableSelectMode.Multiple;
         script.useDynamicAttach = true;
-       
+
+        //obj.GetComponent<Rigidbody>().isKinematic = false;
+        //obj.GetComponent<BoxCollider>().isTrigger = false;
+
     }
     void AddComponentForLemon(GameObject obj)
     {
@@ -137,6 +177,9 @@ public class SliceObj : MonoBehaviour
 
         script.selectMode = InteractableSelectMode.Multiple;
         script.useDynamicAttach = true;
+
+        obj.GetComponent<Rigidbody>().isKinematic = false;
+        obj.GetComponent<BoxCollider>().isTrigger = false;
 
     }
     void AddComponentForWatermelon(GameObject obj)
@@ -154,5 +197,22 @@ public class SliceObj : MonoBehaviour
         script.selectMode = InteractableSelectMode.Multiple;
         script.useDynamicAttach = true;
 
+        obj.GetComponent<Rigidbody>().isKinematic = false;
+        obj.GetComponent<BoxCollider>().isTrigger = false;
+
+    }
+
+    // Titreþim geribildirimini yönetmek için bir fonksiyon
+    void HapticFeedback(InputDevice device, float amplitude, float duration)
+    {
+        HapticCapabilities capabilities;
+        if (device.TryGetHapticCapabilities(out capabilities))
+        {
+            if (capabilities.supportsImpulse)
+            {
+                uint channel = 0; // Kanalý belirle, genellikle 0
+                device.SendHapticImpulse(channel, amplitude, duration);
+            }
+        }
     }
 }
